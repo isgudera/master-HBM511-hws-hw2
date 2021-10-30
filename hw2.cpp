0d399b8fcbc9f96e42cf834f77c7f242e261859a
isgudera <isgudera@gmail.com>
2021-10-30 21:09:40 +0300

    Yenilendi

--- hw2.cpp
+++ hw2.cpp
@@ -17,7 +17,7 @@ void fill_2dStackMatRand(double A[SIZE][SIZE] , int row, int col ) {
     int i,j;
     for(i=0; i<row;i++) {
     	for(j=0 ; j<col; j++){
-    	    A[i][j] = randBtw(0.0 , 1.0);
+    	    A[i][j] = 1 + rand()%10;// randBtw(0.0 , 1.0);
 	}
     }
 
@@ -42,39 +42,61 @@ void transpose_2dStackMat(double A[SIZE][SIZE] , double M[SIZE][SIZE] ,int row, 
 
 }
 
-void getUpper_2dStackMat(double A[SIZE][SIZE], U[SIZE][SIZE],int row, int col ) {
-    double sum=0;
-    for (int i = 0; i < row; ++i)
+void LU_Decomposition_2dStackMat(double A[SIZE][SIZE], double L[SIZE][SIZE] , double U[SIZE][SIZE],int row, int col ) {
+    double sum;
+    for (int i = 0; i < row; i++)
     {
     	for (int j = 0; j < col; j++)
     	{
     	    if (i==j) {
     	    	L[i][j]=1;
     	    }
-    	    if (i<j) {
+    	    else if (i>j) {
     	    	L[i][j]=0;
     	    }
+    	    else {
+                sum = 0; 
+                for (int k = 0; k < i-1; k++)
+                {
+                    sum += L[i][k]*U[k][j];
+                }
+                L[i][j] = (A[i][j] - sum) / U[j][j];
+    	    }
+    	}
+
+        for (int j = 0; j < col; j++)
+        {
     	    if (i>j) {
     	    	U[i][j]=0;
     	    }
-    	    if (i==0) {
-    	    	U[i][j] = A[i][j];
-    	    }
-    	    if (j==0) {
-    	    	L[i][j] = A[i][j];
-    	    }
-
-    	    else {
-
-    	    	for (int k = 0; k < i-1; k++)
-    	    	{
-    	    	    sum += L[i][k]*U[k][j];
-    	    	}
-	    	U[i][j] = A[i][j] - sum;
-    	    }
-    	}
+            else {
+                sum = 0; 
+                for (int k = 0; k < i-1; k++)
+                {
+                    sum += L[i][k]*U[k][j];
+                }
+                U[i][j] = A[i][j] - sum;
+            }
+        }
     }
 }
+
+
+void MatrixMulti_2_2 (double L[SIZE][SIZE] , double U[SIZE][SIZE], double R[SIZE][SIZE], int row, int col ) {
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            for (int k = 0; k < SIZE; k++)
+            {
+                R[i][j] = L[i][k]*U[k][j];
+            }
+        }
+    }
+
+}
+
 
 void print_2DStackMat(double A[SIZE][SIZE] , int row, int col)
 {
@@ -106,11 +128,21 @@ int main(int argc, char const *argv[])
     double A[SIZE][SIZE],transA[SIZE][SIZE], L[SIZE][SIZE], U[SIZE][SIZE], R[SIZE][SIZE];
     fill_2dStackMatRand(A,SIZE,SIZE); // A matrisi oluşturuldu.
     transpose_2dStackMat(A,transA,SIZE,SIZE);
-
+    printf("A Matrix:\n");
     print_2DStackMat(A,SIZE,SIZE);
     //	printf("\n");
 
-    print_2DStackMat(transA,SIZE,SIZE);
+//    print_2DStackMat(transA,SIZE,SIZE);
+
+    LU_Decomposition_2dStackMat(A,L,U,SIZE,SIZE);
+    printf("L Matrix:\n");
+    print_2DStackMat(L,SIZE,SIZE);
+    printf("U Matrix:\n");
+    print_2DStackMat(U,SIZE,SIZE);
+    MatrixMulti_2_2(L,U,R,SIZE,SIZE);
+    printf("R Matrix:\n");
+    print_2DStackMat(R,SIZE,SIZE);
+
 
     printf("Hello world %d\n", SIZE);
     return 0;
