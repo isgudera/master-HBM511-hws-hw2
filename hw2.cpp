fdeeeeb7809909358ad0a6be00175a490ef9ee6a
isgudera <isgudera@gmail.com>
2021-10-30 06:08:41 +0300

    LU decomposition algorithm is upgraded #1

--- hw2.cpp
+++ hw2.cpp
@@ -8,85 +8,111 @@
 using namespace std;
 
 double randBtw(double lb, double ub) {
-
-        double range = (ub - lb);
-        double div = RAND_MAX / range;
-        return lb + (rand() / div);
+    double range = (ub - lb);
+    double div = RAND_MAX / range;
+    return lb + (rand() / div);
 }
 
 void fill_2dStackMatRand(double A[SIZE][SIZE] , int row, int col ) {
-
-        int i,j;
-        for(i=0; i<row;i++) {
-                for(j=0 ; j<col; j++){
-                        A[i][j] = randBtw(0.0 , 1.0);
-                }
-        }
+    int i,j;
+    for(i=0; i<row;i++) {
+    	for(j=0 ; j<col; j++){
+    	    A[i][j] = randBtw(0.0 , 1.0);
+	}
+    }
 
 }
 
 void fill_StackVecRand(double A[SIZE] , int row)
 {
-	for (int i = 0; i < row; i++)
-	{
-		A[i] = randBtw(0.0 , 1.0);
-	}
+    for (int i = 0; i < row; i++)
+    {
+	A[i] = randBtw(0.0 , 1.0);
+    }
 }
 
 void transpose_2dStackMat(double A[SIZE][SIZE] , double M[SIZE][SIZE] ,int row, int col) {
-	for (int i = 0; i < row; i++)
+    for (int i = 0; i < row; i++)
+    {
+	for (int j = 0; j < col; j++)
 	{
-		for (int j = 0; j < col; j++)
-		{
-			M[j][i] = A[i][j];
-		}
+	    M[j][i] = A[i][j];
 	}
+    }
 
 }
 
-void getUpper_2dStackMat(double A[SIZE][SIZE], A[SIZE][SIZE],int row, int col ) {
-	for (int i = 0; i < row; ++i)
-	{
-		/* code */
-	}
+void getUpper_2dStackMat(double A[SIZE][SIZE], U[SIZE][SIZE],int row, int col ) {
+    double sum=0;
+    for (int i = 0; i < row; ++i)
+    {
+    	for (int j = 0; j < col; j++)
+    	{
+    	    if (i==j) {
+    	    	L[i][j]=1;
+    	    }
+    	    if (i<j) {
+    	    	L[i][j]=0;
+    	    }
+    	    if (i>j) {
+    	    	U[i][j]=0;
+    	    }
+    	    if (i==0) {
+    	    	U[i][j] = A[i][j];
+    	    }
+    	    if (j==0) {
+    	    	L[i][j] = A[i][j];
+    	    }
+
+    	    else {
+
+    	    	for (int k = 0; k < i-1; k++)
+    	    	{
+    	    	    sum += L[i][k]*U[k][j];
+    	    	}
+	    	U[i][j] = A[i][j] - sum;
+    	    }
+    	}
+    }
 }
 
 void print_2DStackMat(double A[SIZE][SIZE] , int row, int col)
 {
-	for (int i = 0; i < row; i++)
+    for (int i = 0; i < row; i++)
+    {
+	for (int j = 0; j < col; j++)
 	{
-		for (int j = 0; j < col; j++)
-		{
-			printf("%6.4lf\t", A[i][j]);
-		}
-	printf("\n");
+	    printf("%6.4lf\t", A[i][j]);
 	}
 	printf("\n");
+    }
+    printf("\n");
 
 }
 
 void print_StackVec(double A[SIZE] , int row)
 {
-	printf("\n");
-	for (int i = 0; i < row; i++)
-	{
-		printf("%6.4lf\t", A[i]);
-	}
-	printf("\n");
+    printf("\n");
+    for (int i = 0; i < row; i++)
+    {
+	printf("%6.4lf\t", A[i]);
+    }
+    printf("\n");
 }
 
-int main(int argc, char const *argv[])
+int main(int argc, char const *argv[]) 
 {
+    srand(time(NULL));
+    double A[SIZE][SIZE],transA[SIZE][SIZE], L[SIZE][SIZE], U[SIZE][SIZE], R[SIZE][SIZE];
+    fill_2dStackMatRand(A,SIZE,SIZE); // A matrisi oluşturuldu.
+    transpose_2dStackMat(A,transA,SIZE,SIZE);
 
-	double A[SIZE][SIZE],transA[SIZE][SIZE], L[SIZE][SIZE], U[SIZE][SIZE], R[SIZE][SIZE];
-	fill_2dStackMatRand(A,SIZE,SIZE); // A matrisi oluşturuldu.
-	transpose_2dStackMat(A,transA,SIZE,SIZE);
+    print_2DStackMat(A,SIZE,SIZE);
+    //	printf("\n");
 
-	print_2DStackMat(A,SIZE,SIZE);
-//	printf("\n");
+    print_2DStackMat(transA,SIZE,SIZE);
 
-	print_2DStackMat(transA,SIZE,SIZE);
-
-	printf("Hello world %d\n", SIZE);
-	return 0;
+    printf("Hello world %d\n", SIZE);
+    return 0;
 }
+
